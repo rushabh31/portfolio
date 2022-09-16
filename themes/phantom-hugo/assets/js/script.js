32f2483bfa92bc21c8ea89c8780ee3089c9845ab
Rushabh Patel <50964981+rushabh31@users.noreply.github.com>
2022-09-16 11:43:59 -0400

    Add files via upload

--- themes/phantom-hugo/assets/js/script.js
+++ themes/phantom-hugo/assets/js/script.js
@@ -96,3 +96,30 @@
 })(jQuery);
 
 
+$(function () {
+    
+    var maxL = 200;
+    
+    $('.content').each(function () {
+        
+        var text = $(this).text();
+        if(text.length > maxL) {
+            
+            var begin = text.substr(0, maxL),
+                end = text.substr(maxL);
+
+            $(this).html(begin)
+                .append($('<a class="readmore"/>').attr('href', '#').html('read more...'))
+                .append($('<div class="hidden" />').html(end));
+                
+            
+        }
+    });
+            
+    $(document).on('click', '.readmore', function () {
+				// $(this).next('.readmore').fadeOut("400");
+        $(this).next('.hidden').slideToggle(400);
+    })        
+    
+    
+})
